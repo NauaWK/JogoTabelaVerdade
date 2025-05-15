6655cf76cf4a28ddf00570fb12b4524b2540db63
Nauan WK <106595596+Nauan-WK@users.noreply.github.com>
2025-05-15 20:10:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,149 @@
-const startButton = document.getElementById("startButton")
-const expressionsPanel = document.getElementById("expressionsPanel")
-const trueButton = document.getElementById("trueButton")
-const falseButton = document.getElementById("falseButton")
-const mainText = document.getElementById("mainText")
-const resetButton = document.getElementById("resetButton")
-const expressions = document.getElementById("expressions")
-const motivationalText = document.getElementById("motivationalText")
-const spanPlacar = document.createElement("span");
-const questionCounter = document.getElementById("questionCounter")
-const difficultyPanel = document.getElementById("difficultyPanel")
-const chooseDifficultyText = document.getElementById("chooseDifficultyText")
-const possibleValues = {true: "V", false: "F"}
-
-let gameState = {
-  questionNumber: 0,
-  placar: 0,
-  contador: 0,
-  result: null,
-  difficultyLevel: null
+const possibleValues = {
+  true: "V", 
+  false: "F"
 }
-
+const uiElements = {
+  expressionsPanel: document.getElementById("expressionsPanel"),
+  mainText: document.getElementById("mainText"),
+  expressions: document.getElementById("expressions"),
+  motivationalText: document.getElementById("motivationalText"),
+  difficultyPanel: document.getElementById("difficultyPanel"),
+  chooseDifficultyText: document.getElementById("chooseDifficultyText"),
+  questionCounter: document.getElementById("questionCounter")
+}
+const mainButtons = {
+  startButton: document.getElementById("startButton"),
+  trueButton: document.getElementById("trueButton"),
+  falseButton: document.getElementById("falseButton"),
+  resetButton: document.getElementById("resetButton")
+}
 const difficultyButtons = {
   easyButton: document.getElementById("easy"), 
   mediumButton: document.getElementById("medium"),
   hardButton: document.getElementById("hard")
 }
+let gameState = {
+  questionNumber: 0,
+  placar: 0,
+  contador: 0,
+  result: null,
+  difficultyLevel: 0
+}
+let expressionsVariables = {
+  variable1: null,
+  variable2: null,
+  randomConective: null, 
+  hasNotConective: false, 
+  hasNotConective1: false,
+  hasNotConective2: false,
+  notOperator: "",
+  notOperator1: "",
+  notOperator2: ""
+};
 
+let originalConectivesList = ["AND", "OR", "→", "XOR"]
+let modifiedConectivesList
+
+// habilitar a visualização da tela de seleção de dificuldades ao clicar no botão de Iniciar
+function showDifficultyPanelElements(){
+  toggleVisibility([uiElements.difficultyPanel], "flex")
+  toggleVisibility([uiElements.chooseDifficultyText], "block")
+  toggleVisibility([mainButtons.startButton, uiElements.mainText], "none")
+}
+
+// adicionar um ouvinte de eventos ("click") para cada botão de dificuldade e resgatar seu valor(data-value)
 Object.values(difficultyButtons).forEach(button => {
   button.addEventListener("click", event => {
-    gameState.difficultyLevel = event.target.getAttribute("data-value");
-    setDifficulty(gameState.difficultyLevel);
+    gameState.difficultyLevel = Number(event.target.getAttribute("data-value"));
+    setDifficulty(gameState.difficultyLevel); 
   });
 });
 
-let variable1, variable2, randomConective, notText1, notText2, hasNotConective, hasNotConective1, hasNotConective2
-const originalConectives = ["AND", "OR", "→", "XOR"]
-let conectives = [...originalConectives]
+// função para modificar a lista de conectivos de acordo com a dificuldade
 function setDifficulty(level){
-  conectives = [...originalConectives]
-  gameState.difficultyLevel = level
+  modifiedConectivesList = [...originalConectivesList]
   switch(level){
-    case "1": 
-      conectives = conectives.filter(conective => !["→", "XOR"].includes(conective))
+    case 1: 
+      modifiedConectivesList = modifiedConectivesList.filter(conective => !["→", "XOR"].includes(conective))
       break
-    case "2":
-      conectives = conectives.filter(conective => conective !== "XOR")
+    case 2:
+      modifiedConectivesList = modifiedConectivesList.filter(conective => !["XOR"].includes(conective))
       break
-    case "3":
-      conectives = conectives.filter(conective => !["AND", "OR"].includes(conective))
-      break
+    case 3:
+      modifiedConectivesList = modifiedConectivesList.filter(conective => !["AND", "OR"].includes(conective))
+      break  
   }
   startGame()
 }
 
+// função para alterar display dos elementos na página
 function toggleVisibility(elements, displayType) {
   elements.forEach(el => el.style.display = displayType);
 }
-
-function showDifficultyPanel(){
-  toggleVisibility([difficultyPanel], "flex")
-  toggleVisibility([chooseDifficultyText], "block")
-  toggleVisibility([startButton, mainText], "none")
-}
-
+// função para atualizar o contador da questão atual
 function updateQuestionCounter() {
-  questionCounter.innerHTML = `<span style="color:red; font-weight:bold;">${gameState.questionNumber}</span>/5`;
+  uiElements.questionCounter.innerHTML = `<span style="color:red; font-weight:bold;">${gameState.questionNumber}</span>/5`;
 }
+// função para iniciar o jogo
 function startGame(){
-  expressionsPanel.style.display = "block"
-  toggleVisibility([trueButton, falseButton, resetButton, questionCounter], "inline-block");
-  toggleVisibility([startButton, mainText, difficultyPanel, chooseDifficultyText], "none");
+  toggleVisibility([uiElements.expressionsPanel],"block")
+  toggleVisibility([ mainButtons.trueButton, mainButtons.falseButton, mainButtons.resetButton, uiElements.questionCounter], "inline-block");
+  toggleVisibility([mainButtons.startButton, uiElements.mainText, uiElements.difficultyPanel, uiElements.motivationalText, uiElements.chooseDifficultyText], "none");
   updateQuestionCounter()
   generateExpression() 
 }
+// função para gerar as expressões aleatóriamente
 function generateExpression(){
-  randomConective = conectives[Math.floor(Math.random() * conectives.length)]
-  variable1 = Math.random() < 0.5
-  variable2 = Math.random() < 0.5 
+  expressionsVariables.randomConective = modifiedConectivesList[Math.floor(Math.random() * modifiedConectivesList.length)]
+  expressionsVariables.variable1 = Math.random() < 0.5
+  expressionsVariables.variable2 = Math.random() < 0.5 
   let parenthesis1 = ""
   let parenthesis2 = ""
 
-  // gerar NOT dinamicamente a cada rodada com base na dificuldade
+  // gerar NOT dinamicamente nas expressões a cada rodada, com base na dificuldade escolhida
   switch(gameState.difficultyLevel){
-    case "1":
-      hasNotConective = false;
-      hasNotConective1 = Math.random() < 0.2
-      hasNotConective2 = Math.random() < 0.2
+    case 1:
+      expressionsVariables.hasNotConective = false;
+      expressionsVariables.hasNotConective1 = Math.random() < 0.2
+      expressionsVariables.hasNotConective2 = Math.random() < 0.2
       break
-    case "2":
-      hasNotConective = Math.random() < 0.5
-      hasNotConective1 = Math.random() < 0.5
-      hasNotConective2 = Math.random() < 0.5
+    case 2:
+      expressionsVariables.hasNotConective = Math.random() < 0.5
+      expressionsVariables.hasNotConective1 = Math.random() < 0.8
+      expressionsVariables.hasNotConective2 = Math.random() < 0.8
       break
-    case "3":
-      hasNotConective = Math.random() < 0.7
-      hasNotConective1 = hasNotConective2 = true
+    case 3:
+      expressionsVariables.hasNotConective = Math.random() < 0.7
+      expressionsVariables.hasNotConective1 = expressionsVariables.hasNotConective2 = true
       break
   }
-
   const operatorsList = {
     "AND": (a, b) => a && b,
     "OR": (a, b) => a || b,
     "→": (a, b) => !a || b,
     "XOR": (a, b) => a !== b
   }
-  if (operatorsList[randomConective]) {
-    const operator = operatorsList[randomConective]; 
+  if (operatorsList[expressionsVariables.randomConective]) {
+    const operator = operatorsList[expressionsVariables.randomConective]; 
     gameState.result = operator(
-        hasNotConective1 ? !variable1 : variable1,
-        hasNotConective2 ? !variable2 : variable2,
+      expressionsVariables.hasNotConective1 ? !expressionsVariables.variable1 : expressionsVariables.variable1,
+      expressionsVariables.hasNotConective2 ? !expressionsVariables.variable2 : expressionsVariables.variable2
     );
   }
-  notOperator = hasNotConective ? "NOT" : ""
-  if(hasNotConective){
+   expressionsVariables.notOperator = expressionsVariables.hasNotConective ? "NOT" : ""
+  if(expressionsVariables.hasNotConective){
     parenthesis1 = "("
     parenthesis2 = ")"
     gameState.result = !gameState.result
   } 
-  notOperator1 = hasNotConective1 ? "NOT" : ""
-  notOperator2 = hasNotConective2 ? "NOT" : ""
+  
+  expressionsVariables.notOperator1 = expressionsVariables.hasNotConective1 ? "NOT" : ""
+  expressionsVariables.notOperator2 = expressionsVariables.hasNotConective2 ? "NOT" : ""
   
   // expressão gerada aleatóriamente
-  expressions.textContent = `Q = ${possibleValues[variable1]}, P = ${possibleValues[variable2]}, qual o resultado de ${notOperator} ${parenthesis1} ${notOperator1} Q ${randomConective} ${notOperator2} P ${parenthesis2} ?`
+  uiElements.expressions.textContent = `Q = ${possibleValues[expressionsVariables.variable1]}, P = ${possibleValues[expressionsVariables.variable2]}, qual o resultado de ${expressionsVariables.notOperator} ${parenthesis1} ${expressionsVariables.notOperator1} Q ${expressionsVariables.randomConective} ${expressionsVariables.notOperator2} P ${parenthesis2} ?`
 }
-function resetGame(){
-  mainText.style.display = "block"
-  toggleVisibility([startButton], "inline-block");
-  toggleVisibility([expressionsPanel, trueButton, falseButton, resetButton, motivationalText,questionCounter], "none");
-  expressions.textContent = ""
-  questionCounter.innerHTML = ""
-  gameState.contador = 0
-  gameState.placar = 0
-  gameState.questionNumber = 0
-}
+// função para verificar a resposta do jogador
 function verifyAnswer(event){
   let userAnswer = event.target.getAttribute("data-value") === "true"
 
@@ -145,18 +156,16 @@ function verifyAnswer(event){
   gameState.questionNumber ++
   updateQuestionCounter()
   gameState.contador ++
+
   if(gameState.contador < 5){
     generateExpression()
   }
   else{
-    // alterar o estilo apenas dos pontos do placar
-    spanPlacar.textContent = gameState.placar; 
-    spanPlacar.style.color = "red";
-    spanPlacar.style.fontWeight = "bold"; 
-    expressions.innerText = "Placar final: "
-    expressions.appendChild(spanPlacar)
-    toggleVisibility([trueButton, falseButton], "none")
+    uiElements.expressions.innerHTML = `Placar final: <span style="color: red; font-weight: bold;"> ${gameState.placar} </span>`
+    toggleVisibility([mainButtons.trueButton, mainButtons.falseButton], "none")
+    toggleVisibility([uiElements.motivationalText], "inline-block")
     motivationalText.style.display = "block"
+
     switch(gameState.placar){
       case 0:
         motivationalText.textContent = "Bom, pelo menos você testou se o placar estava funcionando..."
@@ -179,7 +188,18 @@ function verifyAnswer(event){
     }
   }
 }
-startButton.addEventListener("click", showDifficultyPanel)
-trueButton.addEventListener("click", verifyAnswer)
-falseButton.addEventListener("click", verifyAnswer)
-resetButton.addEventListener("click", resetGame)
+// função para reinicar o jogo
+function resetGame(){
+  uiElements.mainText.style.display = "block"
+  toggleVisibility([mainButtons.startButton], "inline-block");
+  toggleVisibility([uiElements.expressionsPanel, mainButtons.trueButton, mainButtons.falseButton, mainButtons.resetButton, motivationalText,questionCounter], "none");
+  uiElements.expressions.textContent = ""
+  questionCounter.innerHTML = ""
+  gameState.contador = 0
+  gameState.placar = 0
+  gameState.questionNumber = 0
+}
+mainButtons.startButton.addEventListener("click", showDifficultyPanelElements)
+mainButtons.trueButton.addEventListener("click", verifyAnswer)
+mainButtons.falseButton.addEventListener("click", verifyAnswer)
+mainButtons.resetButton.addEventListener("click", resetGame)
